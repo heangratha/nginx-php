b23f2c19400bfc322332fac7380e33de299c8de0
Ratha Heang <ratha@web-essentials.asia>
2020-01-21 17:07:02 +0700

    Fix nginx load php-fpm 7.4

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,5 +9,5 @@ USER_ID=${LOCAL_USER_ID:-1000}
 usermod -u $USER_ID -o www-data && groupmod -g $USER_ID -o www-data
 
 # Start php-fpm
-/etc/init.d/php7.3-fpm start
+/etc/init.d/php7.4-fpm start
 exec "$@"
